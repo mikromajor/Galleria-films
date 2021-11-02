0e15109c0145e87366d28fd7a5752175973fbc5c
mikromajor <robocza.sowa@yandex.com>
2021-11-02 17:48:56 +0200

    3-Uncaught SyntaxError: Unexpected token <   brands.js:1

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,10 +2,17 @@ import { useState } from "react";
 import { HeadMenu } from "./HeadMenu";
 import CardsFilms from "./CardsFilms/CardsFilms";
 
-import "@fortawesome/fontawesome-free/css/all.css";
+//import "@fortawesome/fontawesome-free/css/all.css";
+//
+//перед bootstrap из pack.json
+// "@fortawesome/fontawesome-free": "^5.15.4",
+//     "@testing-library/jest-dom": "^5.14.1",
+//     "@testing-library/react": "^11.2.7",
+//     "@testing-library/user-event": "^12.8.3",
 
 function App() {
   const [cardsData, setCardsData] = useState(null);
+
   document.body.classList.add("bgBody");
   return (
     <>
